294573b2005e772086500d4086c7863ed1ccd8cf
Sébastien Combéfis <seb478@gmail.com>
2015-08-24 20:48:46 +0200

    Update Python rootfs configuration
    - Changed package to use due to a migration to Debian jessie

--- python/rootfs-config.sh
+++ python/rootfs-config.sh
@@ -1,4 +1,4 @@
-# Copyright 2013 The Pythia Authors.
+# Copyright 2015 The Pythia Authors.
 # This file is part of Pythia.
 #
 # Pythia is free software: you can redistribute it and/or modify
@@ -14,11 +14,12 @@
 # along with Pythia.  If not, see <http://www.gnu.org/licenses/>.
 
 # Python 3.1
-install_debs python3.1 python3-minimal python3.1-minimal
+install_debs python3.4 python3-minimal python3.4-minimal libpython3.4-stdlib libpython3.4-minimal
 
 # Base libraries
 install_debs libc6 libc-bin libgcc1
 
 # Additional libraries
-install_debs libexpat1 libssl0.9.8 zlib1g libbz2-1.0 libdb4.8 libncursesw5 \
-             libreadline6 libncurses5 readline-common libsqlite3-0 mime-support
+install_debs libexpat1 libssl1.0.0 zlib1g libbz2-1.0 libdb5.3 libncursesw5 \
+             libreadline6 readline-common libsqlite3-0 mime-support \
+             libffi6 liblzma5 libmpdec2 libtinfo5 multiarch-support
