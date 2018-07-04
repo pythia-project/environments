fbc23731c9b84475dc1e3d8e6e325f9e898102ec
Sébastien Combéfis <seb478@gmail.com>
2018-07-04 13:15:56 +0200

    Update Python rootfs to version 3.5

--- python/rootfs-config.sh
+++ python/rootfs-config.sh
@@ -13,13 +13,13 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Pythia.  If not, see <http://www.gnu.org/licenses/>.
 
-# Python 3.1
-install_debs python3.4 python3-minimal python3.4-minimal libpython3.4-stdlib libpython3.4-minimal
+# Python 3.5
+install_debs python3.5 python3-minimal python3.5-minimal libpython3.5-stdlib libpython3.5-minimal
 
 # Base libraries
 install_debs libc6 libc-bin libgcc1
 
 # Additional libraries
-install_debs libexpat1 libssl1.0.0 zlib1g libbz2-1.0 libdb5.3 libncursesw5 \
-             libreadline6 readline-common libsqlite3-0 mime-support \
+install_debs libexpat1 libssl1.0.2 zlib1g libbz2-1.0 libdb5.3 libncursesw5 \
+             libreadline7 readline-common libsqlite3-0 mime-support \
              libffi6 liblzma5 libmpdec2 libtinfo5 multiarch-support
