3c136d161103f0efa17d374af80d7f3da7fc1721
Sébastien Combéfis <seb478@gmail.com>
2018-07-22 16:03:34 +0200

    Update Java rootfs to version 8

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Available environments are built with *Debian stretch* packages.
 ## Available environments
 
 - Busybox
-- Java (OpenJDK 7)
+- Java (OpenJDK 8)
 - Mono (Mono 3.2.8)
 - Python (Python 3.5.3-1)
 - C (gcc 4.9)
--- java/rootfs-config.sh
+++ java/rootfs-config.sh
@@ -1,4 +1,4 @@
-# Copyright 2015 The Pythia Authors.
+# Copyright 2018 The Pythia Authors.
 # This file is part of Pythia.
 #
 # Pythia is free software: you can redistribute it and/or modify
@@ -16,16 +16,12 @@
 # Install busybox
 install_busybox
 
-# Java OpenJDK7
-install_debs openjdk-7-jdk openjdk-7-jre java-common openjdk-7-jre-lib openjdk-7-jre-headless
+# Java OpenJDK 8
+install_debs openjdk-8-jdk openjdk-8-jdk-headless java-common \
+             openjdk-8-jre openjdk-8-jre-headless
 
 # Base libraries
-install_debs libc6 libglib2.0-0 libgcc1 libstdc++6
+install_debs libc6 libgcc1 libstdc++6
 
 # Additional libraries
-install_debs zlib1g libasound2 libatk-wrapper-java-jni libatk1.0-0 libcairo2 \
-             libcups2 libfontconfig1 libfreetype6 libgdk-pixbuf2.0-0 libgif4 \
-             libgl1-mesa-glx libgtk2.0-0 libjpeg62-turbo libpango-1.0-0 \
-             libpangocairo-1.0-0 libpangoft2-1.0-0 libpng12-0 \
-             multiarch-support ca-certificates-java libkrb5-3 liblcms2-2 \
-             libnss3 libpcsclite1 libpulse0 tzdata-java
+install_debs zlib1g
